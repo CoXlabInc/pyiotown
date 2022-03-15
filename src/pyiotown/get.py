1a00533ed2792d879fabb0bddc12a028839ce256
Jongsoo Jeong <jsjeong@coxlab.kr>
2022-03-15 15:55:20 +0900

    GNBS-68 Added the group_id arg into the storage() for administrators. Fixed a typo.

--- src/pyiotown/get.py
+++ src/pyiotown/get.py
@@ -9,7 +9,7 @@ def downloadAnnotations(url, token, classname, verify=True, timeout=60):
     apiaddr = url + "/api/v1.0/nn/images?labels=" + classname
     header = {'Accept':'application/json', 'token':token}
     try:
-        r = requests.get(apiaddr, headers=header, verify=verify, timeout=timeout
+        r = requests.get(apiaddr, headers=header, verify=verify, timeout=timeout)
         if r.status_code == 200:
             return r.json()
         else:
@@ -19,14 +19,20 @@ def downloadAnnotations(url, token, classname, verify=True, timeout=60):
         print(e)
         return None
 
-def storage(url, token, nid, date_from , date_to, lastKey="", verify=True, timeout=60):
+def storage(url, token, nid, date_from , date_to, lastKey="", group_id=None, verify=True, timeout=60):
     '''
     url : IoT.own Server Address
     token : IoT.own API Token
     nid : Node ID
     date_from : UTC structure ex) "2018-11-02T13:00:00Z"
     '''
+                    
     header = {'Accept':'application/json','token':token}
+
+    # only for administrators
+    if group_id is not None:
+        header['grpid'] = group_id
+    
     apiaddr = url + "/api/v1.0/storage?nid=" + nid + "&from=" + date_from + "&to=" + date_to
     if lastKey != "":
         apiaddr = url + "/api/v1.0/storage?nid=" + nid + "&from=" + date_from + "&to=" + date_to + "&lastKey=" + lastKey
