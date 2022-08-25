97a9417ae707fe8770b083f7a68c4e81ecc096d0
Jongsoo Jeong <jsjeong@coxlab.kr>
2022-08-25 22:57:54 +0900

    Fixed to use pbr.

--- setup.py
+++ setup.py
@@ -1,27 +1,5 @@
+#!/usr/bin/env python3
+
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pyiotown",
-    version="0.3.0",
-    author="CoXlab Inc.",
-    author_email="support@coxlab.kr",
-    description="IoT.own API",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/CoXlabInc/pyiotown",
-    project_urls={
-        "Bug Tracker": "https://github.com/CoXlabInc/pyiotown/issues",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
-    install_requires=["setuptools>=42","wheel","requests","paho-mqtt"],
-)
+setuptools.setup(setup_requires=['pbr'], pbr=True)
