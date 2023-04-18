d55170ef82927108927b3b6ed8971a083e7efc08
Jongsoo Jeong <jsjeong@coxlab.kr>
2023-04-18 15:00:05 +0900

    NOLTIS-359 Fixed a typo.

--- src/pyiotown/post_process.py
+++ src/pyiotown/post_process.py
@@ -34,7 +34,7 @@ def on_message(client, userdata, msg):
         print(f'Error on calling the user-defined function', file=sys.stderr)
         print(e, file=sys.stderr)
 
-        message['pp_error'][message['pp_list'][0]['name']] = f"Error on post process ({e})"
+        message['pp_error'][message['pp_list'][0]] = f"Error on post process ({e})"
 
         if userdata['dry'] == False:
             client.publish('iotown/proc-done', json.dumps(message), 1)
